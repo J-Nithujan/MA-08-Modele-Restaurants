e80432327df36aae274fa02cbad3b45ed697efd8
Jeffrey Mvutu Mabilama <jeffrey.mvutu@gmail.com>
2022-01-28 13:40:41 +0100

    data insertion

--- travail/ordre-insertion-donnees-dans-tables.sql
+++ travail/ordre-insertion-donnees-dans-tables.sql
@@ -8,32 +8,32 @@
 # =======================================================
 
 # ordre 1 validé
-INSERT [dbo].[sales] -- ok
-INSERT [dbo].[allergens] -- ok
-INSERT [dbo].[dishes_types] -- ok
-INSERT [dbo].[measurement_units] -- ok
-INSERT [dbo].[locations] -- ok
-INSERT [dbo].[restaurant_types] -- ok
+INSERT [dbo].[sales] -- ok (import bdd ok)
+INSERT [dbo].[allergens] -- ok (import bdd ok)
+INSERT [dbo].[dishes_types] -- ok (import bdd ok)
+INSERT [dbo].[measurement_units] -- ok (import bdd ok)
+INSERT [dbo].[locations] -- ok (import bdd ok)
+INSERT [dbo].[restaurant_types] -- ok (import bdd ok)
 
 # ordre 2 validé
-INSERT [dbo].[dishes]
-INSERT [dbo].[ingredients]
-INSERT [dbo].[users]
-INSERT [dbo].[restaurants]
+INSERT [dbo].[dishes] -- /
+INSERT [dbo].[ingredients] -- /
+INSERT [dbo].[users] -- /
+INSERT [dbo].[restaurants] -- /
 
 # ordre 3 validé
-INSERT [dbo].[dish_has_allergen]
-INSERT [dbo].[dish_has_ingredient]
-INSERT [dbo].[orders]
-INSERT [dbo].[restaurant_has_dish]
-INSERT [dbo].[restaurant_has_type_of_restaurant]
-INSERT [dbo].[restaurant_has_sale]
-INSERT [dbo].[restaurant_has_user]
-INSERT [dbo].[reviews]
+INSERT [dbo].[dish_has_allergen] -- 500
+INSERT [dbo].[dish_has_ingredient] -- 500
+INSERT [dbo].[orders] -- 100
+INSERT [dbo].[restaurant_has_dish] -- 500 -- ok
+INSERT [dbo].[restaurant_has_type_of_restaurant] -- 450
+INSERT [dbo].[restaurant_has_sale] -- 350 -- ok
+INSERT [dbo].[restaurant_has_user] -- 300
+INSERT [dbo].[reviews] -- 200
 
 # ordre 3+ / 4 validé
-INSERT [dbo].[order_details]
-INSERT [dbo].[order_detail_has_ingredient]
+INSERT [dbo].[order_details] -- 200
+INSERT [dbo].[order_detail_has_ingredient] -- 200 -- ok
 
 
 # =======================================================
