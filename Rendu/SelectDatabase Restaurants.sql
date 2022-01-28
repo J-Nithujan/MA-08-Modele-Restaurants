bd0190a0531c87183447b420ce7d3389c3be9c4c
J-Nithujan <nithujan.jegatheeswaran@cpnv.ch>
2022-01-28 16:58:56 +0100

    Update SelectDatabase Restaurants.sql

--- Rendu/SelectDatabase Restaurants.sql
+++ Rendu/SelectDatabase Restaurants.sql
@@ -1,28 +1,28 @@
-SELECT * from dbo.allergens
-SELECT * from dbo.dish_has_allergen
-SELECT * from dbo.dish_has_ingredient
-SELECT * from dbo.dishes
-SELECT * from dbo.dishes_types
-SELECT * from dbo.ingredients
-SELECT * from dbo.locations
-SELECT * from dbo.measurement_units
-SELECT * from dbo.order_detail_has_ingredient
-SELECT * from dbo.order_details
-SELECT * from dbo.orders
-SELECT * from dbo.restaurant_has_dish
-SELECT * from dbo.restaurant_has_sale
-SELECT * from dbo.restaurant_has_type_of_restaurant
-SELECT * from dbo.restaurant_has_user
-SELECT * from dbo.restaurant_types
-SELECT * from dbo.restaurants
-SELECT * from dbo.reviews
-SELECT * from dbo.sales
-SELECT * from dbo.users
+SELECT * from [db_restaurants].[dbo].allergens
+SELECT * from [db_restaurants].[dbo].dish_has_allergen
+SELECT * from [db_restaurants].[dbo].dish_has_ingredient
+SELECT * from [db_restaurants].[dbo].dishes
+SELECT * from [db_restaurants].[dbo].dishes_types
+SELECT * from [db_restaurants].[dbo].ingredients
+SELECT * from [db_restaurants].[dbo].locations
+SELECT * from [db_restaurants].[dbo].measurement_units
+SELECT * from [db_restaurants].[dbo].order_detail_has_ingredient
+SELECT * from [db_restaurants].[dbo].order_details
+SELECT * from [db_restaurants].[dbo].orders
+SELECT * from [db_restaurants].[dbo].restaurant_has_dish
+SELECT * from [db_restaurants].[dbo].restaurant_has_sale
+SELECT * from [db_restaurants].[dbo].restaurant_has_type_of_restaurant
+SELECT * from [db_restaurants].[dbo].restaurant_has_user
+SELECT * from [db_restaurants].[dbo].restaurant_types
+SELECT * from [db_restaurants].[dbo].restaurants
+SELECT * from [db_restaurants].[dbo].reviews
+SELECT * from [db_restaurants].[dbo].sales
+SELECT * from [db_restaurants].[dbo].users
 
 
 -- Permettre de rechercher des restaurants:
 -- - avec un bon avis (reviews)
-SELECT * FROM dbo.restaurants AS resto
-    INNER JOIN dbo.reviews AS avis ON dbo.reviews.restaurant_id = resto.id
+SELECT * FROM [db_restaurants].[dbo].restaurants AS resto
+    INNER JOIN [db_restaurants].[dbo].reviews AS avis ON reviews.restaurant_id = resto.id
     WHERE avis.rating >= 4;
 
